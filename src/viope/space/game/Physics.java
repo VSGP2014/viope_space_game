df3503fb619f9a8d98f6b7d1f60b7ed4d3668a12
Giovanni Santini <giovannisantini93@yahoo.it>
2014-04-07 18:45:41 +0200

    Adding stub Physics class Waiting for correct one

---
+++ src/viope/space/game/Physics.java
@@ -0,0 +1,12 @@
+package viope.space.game;
+
+/**
+ * @author      Giovanni Santini	<giovanni.santini@studenti.unimi.it>
+ * @version     0.1
+ * @since       2014-04-07
+ * This class should implement methods used by phisical objects
+ */
+
+public class Physics {
+
+}
