f715cebcf0d67482409149ff2a29975b2bf3f23b
Giovanni Santini <giovannisantini93@yahoo.it>
2014-04-10 18:32:05 +0200

    Some coding corrections

--- src/viope/space/game/SpaceShip.java
+++ src/viope/space/game/SpaceShip.java
@@ -5,20 +5,20 @@ import java.awt.Color;
 public class SpaceShip extends Body{
 
 	private int energy;
-	private boolean launched=false;
+	private boolean launched;
+	
 	public SpaceShip(double mass, int x, int y, Color c, int energy) {
 		super(mass, x, y, c);
 		this.energy=energy;
-		
+		this.launched=false;
 	}
-	public void Launch()
-	{
+	
+	public void launch() {
 		launched=true;
 	}
-	public void update()
-	{
-		if(launched)
-		{
+	
+	public void update(){
+		if(this.launched){
 			this.energy--;
 		}
 	}
