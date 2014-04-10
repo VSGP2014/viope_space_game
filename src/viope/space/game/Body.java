942f3d3fcfe34c5828141a7577fa9823b84ec1ec
Giovanni Santini <giovannisantini93@yahoo.it>
2014-04-10 21:36:17 +0200

    Updated Body class
    Still have to javadoc it and fix/ident it properly

--- src/viope/space/game/Body.java
+++ src/viope/space/game/Body.java
@@ -5,46 +5,55 @@ import java.awt.Graphics;
 
 /**
  * @author      Michael Etzel	<524428@student.inholland.nl>
- * @version     0.1
+ * @version     0.2
  * @since       2014-04-03
  * This class should implement methods used by phisical objects
  */
 
 public class Body extends Physics
 {
+	/**
+	 * Meters per pixel ratio
+	 */
+	static public double scale = 737500000;
 	
 	/**
-	 * Describes the Body's mass
+	 * The Body's name
+	 */
+	private String name;
+	
+	/**
+	 * The Body's mass
 	 */
 	private double mass;
 	
 	/**
-	 * Describes the position on an X axis of a Body
+	 * The position on an X axis of a Body
 	 */
 	private int x;
 	
 	/**
-	 * Describes the position on an Y axis of a Body
+	 * The position on an Y axis of a Body
 	 */
 	private int y;
 	
 	/**
-	 * Describes the velocity on an X axis of a Body
+	 * The velocity on an X axis of a Body
 	 */
-	private double xVel;
+	protected double xVel;
 	
 	/**
-	 * Describes the velocity on an Y axis of a Body
+	 * The velocity on an Y axis of a Body
 	 */
-	private double yVel;
+	protected double yVel;
 	
 	/**
-	 * Describes the accelleration on an X axis of a Body
+	 * The accelleration on an X axis of a Body
 	 */
 	private double xAcc;
 	
 	/**
-	 * Describes the accelleration on an Y axis of a Body
+	 * The accelleration on an Y axis of a Body
 	 */
 	private double yAcc;
 	
@@ -62,8 +71,8 @@ public class Body extends Physics
 	 * @param y		The Y initial position of the Body
 	 * @param c		The color of the Body
 	 */
-	public Body(double mass, int x, int y, Color c)
-	{
+	public Body(String name, double mass, int x, int y, Color c) {
+		this.name = name;
 		this.mass = mass;
 		this.x = x;
 		this.y = y;
@@ -71,16 +80,20 @@ public class Body extends Physics
 	}
 	
 	/**
-	 * A test method used to update variables.
+	 * Using Physics class, updates the actual Body position.
 	 */
-	public void update()
-	{
-		this.xAcc = 0;
-		this.yAcc = 0;
-		this.xVel = 10;
-		this.yVel = 15;
-		this.x += this.xVel;
-		this.y += this.yVel;
+	public void update() {
+		double xf = Physics.sumForce(Physics.getNetXForce(this));
+		double yf = Physics.sumForce(Physics.getNetYForce(this));
+		this.setXAcc(xf / this.mass);
+		this.setXVel(this.getXVel() + this.getXAcc());
+		this.setX(this.getX() + this.getXVel());
+		
+		this.setYAcc(yf / this.mass);
+		this.setYVel(this.getYVel() + this.getYAcc());
+		this.setY(this.getY() + this.getYVel());
+		
+		Physics.clearForces();
 	}
 	
 	/**
@@ -90,7 +103,28 @@ public class Body extends Physics
 	public void draw(Graphics g)
 	{
 		g.setColor(this.color);
-		g.fillOval(this.x, this.y, 50, 50);
+		g.fillOval(this.x - 15, this.y - 15, 30, 30);
+	}
+	
+	/**
+	 * To write
+	 * @param target
+	 * @return
+	 */
+	public double getTargetDistance(Body target) {
+		double x = Math.abs(this.getX() - target.getX());
+		double y = Math.abs(this.getY() - target.getY());
+		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		
+		return distance * scale;
+	}
+	
+	/**
+	 * To write
+	 * @return
+	 */
+	public String getName() {
+		return this.name;
 	}
 	
 	/**
@@ -118,5 +152,55 @@ public class Body extends Physics
 	public double getY()
 	{
 		return this.y;
+	}
+	
+	public void setX(double num)
+	{
+		this.x = (int) num;
+	}
+	
+	public void setY(double num)
+	{
+		this.y = (int) num;
+	}
+	
+	public double getXVel()
+	{
+		return this.xVel;
+	}
+	
+	public void setXVel(double num)
+	{
+		this.xVel = num;
+	}
+	
+	public double getYVel()
+	{
+		return this.yVel;
+	}
+	
+	public void setYVel(double num)
+	{
+		this.yVel = num;
+	}
+	
+	public double getXAcc()
+	{
+		return this.xAcc;
+	}
+	
+	public void setXAcc(double num)
+	{
+		this.xAcc = num;
+	}
+	
+	public double getYAcc()
+	{
+		return this.yAcc;
+	}
+	
+	public void setYAcc(double num)
+	{
+		this.yAcc = num;
 	}
 }
