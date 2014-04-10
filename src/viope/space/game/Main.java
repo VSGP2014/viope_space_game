b91194f7ee2082ac0660ae191fa07368a99591a5
Sarugami <Nikke.tapanainen@metropolia.fi>
2014-04-10 16:37:41 +0300

    Added the Space ship class

--- src/viope/space/game/Main.java
+++ src/viope/space/game/Main.java
@@ -1,10 +1,17 @@
 package viope.space.game;
 
+import java.awt.Color;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SpaceShip ship;
+		Scanner input=new Scanner(System.in);
+		Color shipColor=new Color(0, 0, 0);		
+		System.out.print("Give the amount of energy used to launch the ship: ");
+		int energy=input.nextInt();
+		ship=new SpaceShip(100,25,25,shipColor, energy);
 	}
-
 }
---
+++ src/viope/space/game/SpaceShip.java
@@ -0,0 +1,25 @@
+package viope.space.game;
+
+import java.awt.Color;
+
+public class SpaceShip extends Body{
+
+	private int energy;
+	private boolean launched=false;
+	public SpaceShip(double mass, int x, int y, Color c, int energy) {
+		super(mass, x, y, c);
+		this.energy=energy;
+		// TODO Auto-generated constructor stub
+	}
+	public void Launch()
+	{
+		launched=true;
+	}
+	public void update()
+	{
+		if(launched)
+		{
+			this.energy--;
+		}
+	}
+}
