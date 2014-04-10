1aa8181759b16fd7e77a2638741db092ccb8dc7f
Giovanni Santini <giovannisantini93@yahoo.it>
2014-04-10 18:34:25 +0200

    Use Integer.parseInt instead of Scanner.nextInt

--- src/viope/space/game/Main.java
+++ src/viope/space/game/Main.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		
 		SpaceShip ship;
 		Scanner input=new Scanner(System.in);
+		
 		Color shipColor=new Color(0, 0, 0);		
 		System.out.print("Give the amount of energy used to launch the ship: ");
-		int energy=input.nextInt();
-		ship=new SpaceShip(100,25,25,shipColor, energy);
+		int energy=Integer.parseInt(input.nextLine());
+		ship=new SpaceShip(100, 25, 25, shipColor, energy);
 	}
 }
