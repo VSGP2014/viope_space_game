384b0728a4529ac98c8eee0f536e958b954b6611
utrevi <utrevi@hotmail.com>
2014-04-15 23:24:53 +0200

    Update Planet.java
    The Constructor had a Body as parameter  ... it doesn't look good 
    I don't know if the last 2 rows are useful thus I left them there...

--- src/viope/space/game/Planet.java
+++ src/viope/space/game/Planet.java
@@ -4,12 +4,11 @@ import java.awt.Color;
 
 public class Planet extends Body
 {	
-	public Planet(String name, double mass, Color c, Body parent, double iVel, double peri)
+	public Planet(String name, double mass, Color c, int x, int y, double iVel, double peri)
 	{
-		super(name, mass, parent.getX(), parent.getY(), c);
+		x = (int)(x + (peri / Body.scale));
+		super(name, mass, x, y, c);
 		
-		this.setX((int)(parent.getX() + (peri / Body.scale)));
-		this.setY(parent.getY());
 		this.xVel = 0;
 		this.yVel = -1 * iVel;
 	}
