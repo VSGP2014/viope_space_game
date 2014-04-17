9228237a8c98414de10c1b8a9118a7b27ce57234
Giovanni Santini <giovannisantini93@yahoo.it>
2014-04-17 18:14:36 +0200

    Working on the UI
    - Added a Constats class <-> Any costant should be put here
    - Added a dummy main method <-> After working on other stuff, 
    I'll tweak it

---
+++ src/viope/space/game/Costants.java
@@ -0,0 +1,8 @@
+package viope.space.game;
+
+public class Costants {
+	public static String mainText = 	"What do you want to do?\n"
+									+	"1) Test the game\n"
+									+	"*) Exit"
+									+ "What do you choose?\t";
+}
--- src/viope/space/game/Main.java
+++ src/viope/space/game/Main.java
@@ -6,13 +6,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		SpaceShip ship;
-		Scanner input=new Scanner(System.in);
-		
-		Color shipColor=new Color(0, 0, 0);		
-		System.out.print("Give the amount of energy used to launch the ship: ");
-		int energy=Integer.parseInt(input.nextLine());
-		ship=new SpaceShip(100, 25, 25, shipColor, energy);
+		String mainText = Costants.mainText;		
+		Scanner input = new Scanner(System.in);
+		System.out.print(mainText);
+		int choice = Integer.parseInt(input.nextLine());
+		switch (choice) {
+		case 1:
+			// 1. Game <- will split somewhere if needed
+			SpaceShip ship;
+			Color shipColor=new Color(0, 0, 0);		
+			System.out.print("Give the amount of energy used to launch the ship: ");
+			int energy=Integer.parseInt(input.nextLine());
+			ship=new SpaceShip("Spaceship", 100, 25, 25, shipColor, energy);
+			System.out.print("Will work on it later, sorry :P\n");
+		default:
+			System.out.print("Exit...");
+			return;
+		}
 	}
 }
