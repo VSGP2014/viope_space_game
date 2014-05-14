36a5cada8903ed97937a964bd1e6426f0d076647
Kerabit <makineng@gmail.com>
2014-05-14 18:44:28 +0300

    Complete tests of body class

--- src/model/BodyTest.java
+++ src/model/BodyTest.java
@@ -42,4 +42,59 @@ public class BodyTest {
 		assertEquals(testspeed.getX(),body.getVelocity().getX());
 		assertEquals(testspeed.getY(),body.getVelocity().getY());
 	}
+	
+	@Test
+	public void testBody() {
+		Body body1=new Body("test",new BigDecimal("20"), 100,new Vector(new BigDecimal("20"),new BigDecimal("130")),new Vector(new BigDecimal("10"),new BigDecimal("10")),Color.black);
+		assertNotNull(body1.getName());
+		assertNotNull(body1.getColor());
+		assertNotNull(body1.getLocation());
+		assertNotNull(body1.getSize());
+		assertNotNull(body1.getVelocity());
+		assertNotNull(body1.getWeight());		
+	}
+
+	@Test
+	public void testGetName() {
+		body.setName("testname");
+		String name="testname";
+		assertEquals(body.getName(),name);
+	}
+
+	@Test
+	public void testGetWeight() {
+		body.setWeight(ConstantsUniverse.MARS_MASS);
+		BigDecimal test = ConstantsUniverse.MARS_MASS;
+		assertEquals(test,body.getWeight());
+	}
+
+	@Test
+	public void testGetSize() {
+		body.setSize(256);
+		int testSize = 256;
+		assertEquals(testSize,body.getSize());
+	}
+	
+	@Test
+	public void testMove() {
+		Vector start= new Vector(new BigDecimal("2034.76"), new BigDecimal("5365.87"));
+		body.setLocation(start);
+		body.setVelocity(new Vector(new BigDecimal("10"),new BigDecimal("10")));
+		body.move(new Vector(new BigDecimal("15.15"), new BigDecimal("0.5")));
+		Vector force= new Vector(new BigDecimal("15.15"), new BigDecimal("0.5"));
+		Vector velocity= new Vector(new BigDecimal("10"),new BigDecimal("10"));
+		//assertEquals(start,body.getLocation());
+		velocity=velocity.add(force);
+		start=start.add(velocity);
+		assertEquals(start.getX(),body.getLocation().getX());
+		assertEquals(start.getY(),body.getLocation().getY());
+	}
+	
+	@Test
+	public void testGetDistance(){
+		Body other= new Body("Tests",new BigDecimal("50"),100,new Vector(new BigDecimal("50"),new BigDecimal("50")),new Vector(new BigDecimal("50"),new BigDecimal("50")),Color.BLACK);
+		BigDecimal num= body.getDistance(other);
+		BigDecimal num2 = body.getLocation().getDistance(other.getLocation()); 
+		assertEquals(num,num2);
+	}
 }
