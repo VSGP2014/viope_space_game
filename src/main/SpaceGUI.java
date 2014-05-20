5e304677f324e301017fa119751922559e8ec474
Giovanni Santini <giovannisantini93@yahoo.it>
2014-05-20 14:04:42 +0200

    Using Editor class... For mainly seeing it and testing it

--- src/main/SpaceGUI.java
+++ src/main/SpaceGUI.java
@@ -23,6 +23,7 @@ public class SpaceGUI extends JPanel {
 	
 	public SpaceGUI(){
 		//setBackground(new Color(0, 51, 102)); <- Blue background
+		Editor e = new Editor();
 		setBackground(new Color(0, 0, 0));
 		setLayout(null);
 		KeyListener listener=new MyKeyListener();
