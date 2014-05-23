bf7196a561585bfbce25e69fa02f54e89f102b31
Giovanni Santini <giovannisantini93@yahoo.it>
2014-05-23 18:37:37 +0200

    Use system LookAndFeel

--- src/main/SpaceGame.java
+++ src/main/SpaceGame.java
@@ -1,10 +1,22 @@
 package main;
 import java.awt.EventQueue;
+import javax.swing.*;
 
 public class SpaceGame {
 	// Run the game!
 	public static void main(String[] args){
-		try {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        try {
 		EventQueue.invokeAndWait(MainMenu.createMenu());
 		} catch (Exception e) 
 		{System.out.println("Hello derp"); return;}
